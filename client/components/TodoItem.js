4b0ee722603fb676541513ae50c61e53b5750e65
Stefan Ylimäki <stefan.ylimaki@gmail.com>
2023-01-19 21:21:39 +0200

    added deletion confirmation

--- client/components/TodoItem.js
+++ client/components/TodoItem.js
@@ -1,11 +1,21 @@
-import { View, Text, StyleSheet, Pressable } from "react-native";
+import { View, Text, StyleSheet, Pressable, Alert } from "react-native";
 
 const TodoItem = ({ todo, deleteTodo }) => {
+  const handleClickOnTodo = () => {
+    Alert.alert("Confirm deletion", "Do you really want to delete this todo?", [
+      {
+        text: "Cancel",
+        style: "cancel",
+      },
+      { text: "OK", onPress: () => deleteTodo(todo.item._id) },
+    ]);
+  };
+
   return (
     <View style={styles.todoItem}>
       <Pressable
         android_ripple={{ color: "#210644" }}
-        onPress={deleteTodo.bind(this, todo.item._id)}
+        onPress={handleClickOnTodo}
         style={({ pressed }) => pressed && styles.pressedTodoItem}
       >
         <Text style={styles.todoItemText}>{todo.item.text}</Text>
